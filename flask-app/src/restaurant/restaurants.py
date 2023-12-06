bd6ba850adc1b8db15e69a0ef24ac309f4bc9de4
alinageng <geng.a@northeastern.edu>
2023-12-05 19:49:57 -0500

    finished restaurant reviews endpoint

--- flask-app/src/restaurant/restaurants.py
+++ flask-app/src/restaurant/restaurants.py
@@ -38,14 +38,29 @@ def get_restaurant_detail(id):
 
 @restaurants.route('/restaurants/<restaurant_id>/reviews', methods=['GET'])
 def get_restaurant_reviews(restaurant_id):
+    output = []
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM RestaurantReview WHERE restaurant_id = ' + str(restaurant_id))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
+    cursor.execute('SELECT rr.restaurant_review_id, restaurant_id, description, rating, date, first_name, last_name \
+                    FROM RestaurantReview rr JOIN Customer c on rr.customer_id = c.customer_id \
+                    WHERE rr.restaurant_id = ' + str(restaurant_id))
+
+    review_row_headers = [x[0] for x in cursor.description]
+    reviews = cursor.fetchall()
+    for review_row in reviews:
+        menu_item_reviews_json = []
+        restaurant_review_id = review_row[0]
+        cursor.execute('SELECT description, rating, menu_item_review_id, name FROM MenuItemReview NATURAL JOIN MenuItem WHERE review_id = ' + str(restaurant_review_id))
+
+        menu_item_review_row_headers = [x[0] for x in cursor.description]
+        menu_item_reviews = cursor.fetchall()
+        for menu_item_review_row in menu_item_reviews:
+            menu_item_reviews_json.append(dict(zip(menu_item_review_row_headers, menu_item_review_row)))
+
+        review_row_headers.append("menu_item_reviews")
+        review_row = review_row + (menu_item_reviews_json,)
+        output.append(dict(zip(review_row_headers, review_row)))
+
+    the_response = make_response(jsonify(output))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
