fc58614c8e13843a2bb19fba68c9d9af24b9e8eb
alinageng <geng.a@northeastern.edu>
2023-12-06 20:28:36 -0500

    added menus

--- flask-app/src/restaurant/restaurants.py
+++ flask-app/src/restaurant/restaurants.py
@@ -21,15 +21,30 @@ def get_restaurants_promotions(restaurant_id):
 @restaurants.route('/menu/<menu_item_id>/', methods=['GET'])
 def get_menu_item(menu_item_id):
     cursor = db.get_db().cursor()
-    cursor.execute("SELECT name, price, mi.menu_item_id, menu_type FROM Menu m \
+    cursor.execute("SELECT name, price, mi.menu_item_id, menu_type, menu_id FROM Menu m \
                    JOIN MenuItem mi ON m.menu_item_id = mi.menu_item_id \
-                   WHERE mi.menu_item_id={}".format(menu_item_id))
+                   WHERE m.menu_id={}".format(menu_item_id))
 
-    json_data = []
     theData = cursor.fetchall()
     row_headers = [x[0] for x in cursor.description]
+    json_data = {}
+    for row_header, row_data in zip(row_headers, theData[0]):
+        json_data[row_header] = row_data
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+@restaurants.route('/restaurants/<restaurant_id>/menu_options', methods=['GET'])
+def get_all_menu_items_for_posting_review(restaurant_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(
+        'SELECT name, mi.menu_item_id FROM Menu m JOIN MenuItem mi ON m.menu_item_id = mi.menu_item_id WHERE restaurant_id=' + str(
+            restaurant_id))
+    json_data = []
+    theData = cursor.fetchall()
     for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
+        json_data.append(dict(zip(["label", "value"], row)))
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
@@ -38,10 +53,9 @@ def get_menu_item(menu_item_id):
 @restaurants.route('/restaurants/<restaurant_id>/menu', methods=['GET'])
 def get_all_menu_items(restaurant_id):
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT name, price, mi.menu_item_id, menu_type FROM Menu m \
+    cursor.execute('SELECT name, price, mi.menu_item_id, menu_id, menu_type FROM Menu m \
                    JOIN MenuItem mi ON m.menu_item_id = mi.menu_item_id \
                    WHERE restaurant_id={} ORDER BY menu_type'.format(restaurant_id))
-
 
     json_data = []
     theData = cursor.fetchall()
@@ -89,6 +103,8 @@ def get_restaurant_detail(id):
 
     theData = cursor.fetchall()
     json_data = {}
+    print("LOOK HERE", flush=True)
+    print(theData, flush=True)
     for row_header, row_data in zip(row_headers, theData[0]):
         json_data[row_header] = row_data
     the_response = make_response(jsonify(json_data))
@@ -177,24 +193,16 @@ def delete_restaurant(id):
 
     return 'Success!'
 
-@restaurants.route('/restaurants/<id>/menu', methods=['PUT'])
-def update_restaurant_menu(id):
+@restaurants.route('/menu/<id>', methods=['PUT'])
+def update_menu_item(id):
     the_data = request.json
     current_app.logger.info(the_data)
     print(the_data,flush=True)
-    # address
-    street = the_data['street']
-    address_line_2 = the_data['address_line_2']
-    city = the_data['city']
-    state = the_data['state']
-    # restaurant
-    name = the_data['restaurant_name']
-    description = the_data['description']
-    hours = the_data['hours']
 
-    query = ("UPDATE Restaurant " +
-             "SET restaurant_name = '" + name + "'," + "description = '" + description + "'," + "hours = '" + hours + "' " +
-             "WHERE restaurant_id = " + str(id))
+    price = the_data['price']
+    menu_id = the_data['menu_id']
+
+    query = ("UPDATE Menu SET price = '{}' WHERE menu_id = {}".format(price, menu_id))
     current_app.logger.info(query)
 
     cursor = db.get_db().cursor()
