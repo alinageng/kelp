361b2ccc85016ed4d6fff74bc24e511ae3cfd1a6
edengugsa <eden12gg@gmail.com>
2023-12-06 20:29:35 -0500

    add endpoints for users reviews

--- flask-app/src/restaurant/restaurants.py
+++ flask-app/src/restaurant/restaurants.py
@@ -241,3 +241,106 @@ def add_new_restaurant():
     db.get_db().commit()
 
     return 'Success!'
+
+
+@restaurants.route('/customer/<customer_id>/reviews', methods=['GET'])
+def get_customer_reviews(customer_id):
+    output = []
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT rr.restaurant_review_id, rr.restaurant_id, rr.description, rr.rating, rr.date, c.first_name, c.last_name, rest.restaurant_name \
+                    FROM RestaurantReview rr JOIN Customer c on rr.customer_id = c.customer_id \
+                    JOIN Restaurant rest ON rr.restaurant_id = rest.restaurant_id \
+                    WHERE rr.customer_id = ' + str(customer_id))
+
+    review_row_headers = [x[0] for x in cursor.description]
+    reviews = cursor.fetchall()
+    for review_row in reviews:
+        menu_item_reviews_json = []
+        restaurant_review_id = review_row[0]
+        cursor.execute('SELECT description, rating, menu_item_review_id, name FROM MenuItemReview NATURAL JOIN MenuItem WHERE review_id = ' + str(restaurant_review_id))
+
+        menu_item_review_row_headers = [x[0] for x in cursor.description]
+        menu_item_reviews = cursor.fetchall()
+        for menu_item_review_row in menu_item_reviews:
+            menu_item_reviews_json.append(dict(zip(menu_item_review_row_headers, menu_item_review_row)))
+
+        review_row_headers.append("menu_item_reviews")
+        review_row = review_row + (menu_item_reviews_json,)
+        output.append(dict(zip(review_row_headers, review_row)))
+
+    the_response = make_response(jsonify(output))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@restaurants.route('/restaurants/review/<restaurant_review_id>', methods=['GET'])
+def get_review(restaurant_review_id):
+    output = []
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * \
+                        FROM RestaurantReview RR \
+                       WHERE RR.restaurant_review_id = ' + str(restaurant_review_id))
+
+    review_row_headers = [x[0] for x in cursor.description]
+    reviews = cursor.fetchall()
+    for review_row in reviews:
+        menu_item_reviews_json = []
+        restaurant_review_id = review_row[0]
+        cursor.execute('SELECT description, rating, menu_item_review_id, name FROM MenuItemReview NATURAL JOIN MenuItem WHERE review_id = ' + str(restaurant_review_id))
+
+        menu_item_review_row_headers = [x[0] for x in cursor.description]
+        menu_item_reviews = cursor.fetchall()
+        for menu_item_review_row in menu_item_reviews:
+            menu_item_reviews_json.append(dict(zip(menu_item_review_row_headers, menu_item_review_row)))
+
+        review_row_headers.append("menu_item_reviews")
+        review_row = review_row + (menu_item_reviews_json,)
+        output.append(dict(zip(review_row_headers, review_row)))
+
+    the_response = make_response(jsonify(output[0]))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@restaurants.route('/restaurants/<restaurant_review_id>', methods=['PUT'])
+def update_review(restaurant_review_id):
+    the_data = request.json
+    current_app.logger.info(the_data)
+    
+
+    customer_id = the_data['street']
+    date = the_data['address_line_2']
+    description = the_data['city']
+    rating = the_data['state']
+    restaurant_id = the_data['']
+
+    # restaurant
+    name = the_data['restaurant_name']
+    description = the_data['description']
+    hours = the_data['hours']
+
+    query = ("UPDATE Restaurant " +
+             "SET restaurant_name = '" + name + "'," + "description = '" + description + "'," + "hours = '" + hours + "' " +
+             "WHERE restaurant_id = " + str(id))
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT address FROM Restaurant WHERE restaurant_id =' + str(id))
+    address_id = cursor.fetchone()[0]
+
+    query = ("UPDATE Address " +
+             "SET street = '" + street + "'," + "address_line_2 = '" + address_line_2 + "'," + "city = '" + city +
+             "'," + "state = '" + state + "' " +
+             "WHERE address_id = " + str(address_id))
+    current_app.logger.info(query)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
