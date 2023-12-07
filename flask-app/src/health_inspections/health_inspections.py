e2168053c7b86342330eafa0d23e6bd79b61833a
jesshuang <huang.je@northeastern.edu>
2023-12-07 18:01:05 -0500

    health endpoints

--- flask-app/src/health_inspections/health_inspections.py
+++ flask-app/src/health_inspections/health_inspections.py
@@ -5,10 +5,28 @@ from .. import db
 health_inspections = Blueprint('health_inspections', __name__)
 
 # Get all health inspections from the DB
-@health_inspections.route('/health_inspections', methods=['GET'])
-def get_health_inspections():
+@health_inspections.route('/health_inspections/asc', methods=['GET'])
+def get_health_inspections_asc():
 
-    query = 'SELECT hi.health_inspection_id, hi.inspector_id, hi.restaurant_id, hi.date, hi.grade, r.restaurant_name FROM HealthInspection as hi JOIN Restaurant as r on r.restaurant_id = hi.restaurant_id'
+    query = 'SELECT hi.health_inspection_id, hi.inspector_id, hi.restaurant_id, hi.date, hi.grade, r.restaurant_name FROM HealthInspection as hi JOIN Restaurant as r on r.restaurant_id = hi.restaurant_id order by hi.grade asc'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get all health inspections from the DB
+@health_inspections.route('/health_inspections/desc', methods=['GET'])
+def get_health_inspections_desc():
+
+    query = 'SELECT hi.health_inspection_id, hi.inspector_id, hi.restaurant_id, hi.date, hi.grade, r.restaurant_name FROM HealthInspection as hi JOIN Restaurant as r on r.restaurant_id = hi.restaurant_id order by hi.grade desc'
 
     cursor = db.get_db().cursor()
     cursor.execute(query)
@@ -25,13 +43,15 @@ def get_health_inspections():
 # Get health inspection detail for a restaurant with particular restaurant_id
 @health_inspections.route('/health_inspections/<restaurant_id>', methods=['GET'])
 def get_health_inspection_by_restaurant(restaurant_id):
-    query = 'SELECT * FROM HealthInspection WHERE restaurant_id = ' + str(restaurant_id)
+    query = "SELECT * FROM HealthInspection WHERE restaurant_id = {}".format(restaurant_id)
 
     cursor = db.get_db().cursor()
     cursor.execute(query)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
+    print("LOOK HERE",flush=True)
+    print(theData,flush=True)
     for row in theData:
         json_data.append(dict(zip(row_headers, row)))
     the_response = make_response(jsonify(json_data))
@@ -48,17 +68,15 @@ def add_new_health_inspection():
     current_app.logger.info(the_data)
 
     #extracting the variable
-    inspector_id = the_data['inspector_id']
+    inspector_id = the_data['health_inspector_id']
     restaurant_id = the_data['restaurant_id']
-    date = the_data['date']
     grade = the_data['grade']
 
     # Constructing the query
-    query = 'insert into HealthInspection (inspector_id, restaurant_id, date, grade) values ('
-    query += str(inspector_id) + ', '
-    query += str(restaurant_id) + ', "'
-    query += str(date) + '", '
-    query += str(grade) + ')'
+    query = "insert into HealthInspection (inspector_id, restaurant_id, grade) values ("
+    query += str(inspector_id) + ","
+    query += str(restaurant_id) + ",'"
+    query += str(grade) + "')"
     print(query)
     current_app.logger.info(query)
 
--- flask-app/src/health_inspectors/health_inspectors.py
+++ flask-app/src/health_inspectors/health_inspectors.py
@@ -8,15 +8,19 @@ health_inspectors = Blueprint('health_inspectors', __name__)
 @health_inspectors.route('/health_inspectors', methods=['GET'])
 def get_health_inspectors():
 
-    query = 'SELECT * FROM HealthInspector'
+    query = "SELECT inspector_id, CONCAT(first_name, ' ', last_name) FROM HealthInspector"
 
     cursor = db.get_db().cursor()
     cursor.execute(query)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
+
+
+    print("the data", flush=True)
+    print(theData, flush=True)
     for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
+        json_data.append(dict(zip(["value","label"], row)))
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
